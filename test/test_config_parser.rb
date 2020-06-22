a9838c7bd6228757b9b9cca19f345dad96516e4c
Mihika Bairathi <mihikabairathi@gmail.com>
2020-06-22 14:36:10 -0700

    removed helper functions in test cases

--- test/test_config_parser.rb
+++ test/test_config_parser.rb
@@ -21,7 +21,19 @@ class TestConfigParser < Test::Unit::TestCase
   def test_special_characters_parsed_correctly
     expected = Config::Directive.new(
       name: 'ROOT',
-      directives: [Config::Directive.new(name: 'source', params: create_params_special)]
+      directives: [
+        Config::Directive.new(
+          name: 'source',
+          params: [
+            get_param('@type', 'tail'),
+            get_param('source_host_key', 'host'),
+            get_param('tag', 'test'),
+            get_param('bind', '0.0.0.0'),
+            get_param('dummy', '[{"message":"hello"},{"message":"bye"}]'),
+            get_param('dummy2', '{"message":"again"}')
+          ]
+        )
+      ]
     )
     assert(ConfigParser.proto_config(ConfigParser.parse_config('test/data/special.conf')) == expected)
   end
@@ -31,7 +43,32 @@ class TestConfigParser < Test::Unit::TestCase
       name: 'ROOT',
       directives: [
         Config::Directive.new(name: 'source', params: [get_param('@type', 'dummy'), get_param('tag', 'test.rr')]),
-        Config::Directive.new(name: 'label', args: '@test', directives: [create_match_multiple])
+        Config::Directive.new(
+          name: 'label',
+          args: '@test',
+          directives: [
+            Config::Directive.new(
+              name: 'match',
+              args: 'test.copy',
+              params: [get_param('@type', 'copy')],
+              directives: [
+                Config::Directive.new(
+                  name: 'store',
+                  params: [get_param('@type', 'stdout'), get_param('output', 'json')]
+                ),
+                Config::Directive.new(
+                  name: 'store',
+                  params: [get_param('@type', 'stdout'), get_param('output', 'ltsv')]
+                ),
+                Config::Directive.new(
+                  name: 'buffer',
+                  args: 'time,tag,memory',
+                  params: [get_param('@type', 'memory')]
+                )
+              ]
+            )
+          ]
+        )
       ]
     )
     assert(ConfigParser.proto_config(ConfigParser.parse_config('test/data/multiple.conf')) == expected)
@@ -42,7 +79,20 @@ class TestConfigParser < Test::Unit::TestCase
       name: 'ROOT',
       directives: [
         Config::Directive.new(name: 'source', params: [get_param('@type', 'tail'), get_param('@label', '@raw')]),
-        create_match_emb
+        Config::Directive.new(
+          name: 'match',
+          args: 'test',
+          directives: [
+            Config::Directive.new(
+              name: 'secondary',
+              params: [get_param('@type', 'secondary_file'), get_param('basename', '${tag}_%Y%m%d%L_${message}')]
+            )
+          ],
+          params: [
+            get_param('@type', 'forward'),
+            get_param('command', "ruby -e 'STDOUT.sync = true; proc = ->(){line = STDIN.readline.chomp;}'")
+          ]
+        )
       ]
     )
     assert(ConfigParser.proto_config(ConfigParser.parse_config('test/data/emb_ruby.conf')) == expected)
@@ -53,54 +103,5 @@ class TestConfigParser < Test::Unit::TestCase
   # helper function to create object of message Param
   def get_param(name, value)
     Config::Param.new(name: name, value: value)
-  end
-
-  # helper function to create all the params of object source
-  def create_params_special
-    p1 = get_param('@type', 'tail')
-    p2 = get_param('source_host_key', 'host')
-    p3 = get_param('tag', 'test')
-    p4 = get_param('bind', '0.0.0.0')
-    p5 = get_param('dummy', '[{"message":"hello"},{"message":"bye"}]')
-    p6 = get_param('dummy2', '{"message":"again"}')
-    [p1, p2, p3, p4, p5, p6]
-  end
-
-  # helper function to make Directive object secondary
-  def create_secondary_emb
-    Config::Directive.new(
-      name: 'secondary',
-      params: [
-        get_param('@type', 'secondary_file'),
-        get_param('basename', '${tag}_%Y%m%d%L_${message}')
-      ]
-    )
-  end
-
-  # helper function to make Directive object match
-  def create_match_emb
-    Config::Directive.new(
-      name: 'match',
-      args: 'test',
-      directives: [create_secondary_emb],
-      params: [
-        get_param('@type', 'forward'),
-        get_param('command', "ruby -e 'STDOUT.sync = true; proc = ->(){line = STDIN.readline.chomp;}'")
-      ]
-    )
-  end
-
-  # helper function to create Directive object match
-  def create_match_multiple
-    Config::Directive.new(
-      name: 'match',
-      args: 'test.copy',
-      params: [get_param('@type', 'copy')],
-      directives: [
-        Config::Directive.new(name: 'store', params: [get_param('@type', 'stdout'), get_param('output', 'json')]),
-        Config::Directive.new(name: 'store', params: [get_param('@type', 'stdout'), get_param('output', 'ltsv')]),
-        Config::Directive.new(name: 'buffer', args: 'time,tag,memory', params: [get_param('@type', 'memory')])
-      ]
-    )
   end
 end
